5ed4b4487e3d29d31ce7617719b36d3500f61a4c
Wei Rui <aug828@gmail.com>
2014-04-23 11:19:20 -0400

    more change

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("made some changes");
         System.out.println("some other change");
+        System.out.println("more change");
     }
 }
